bc904891655f1b98b6094c8f20f231e636f83a52
SOI2016 <srinivas_r@hcl.com>
2016-07-20 19:00:31 +0530

    Update shm_que.c

--- Project/shm_que.c
+++ Project/shm_que.c
@@ -1,6 +1,7 @@
 #include "shm_que.h"
  
 CQUEUE *Cir_Que_Ptr;
+SHMPTR Shm_Ptr;
 
 /* Function to check whether the Circular Queue is Full or Not */
 int isFull(CQUEUE *buf)
@@ -25,9 +26,8 @@ int isEmpty(CQUEUE *buf)
 }
 
 /* Function to initialize the Circular Queue */
-void init_cir_Buf(CQUEUE *buf, int shm_fd, void *shm_mem)
+void init_cir_Buf(CQUEUE *buf, int shm_fd)
 {
-	buf->shm_mem	= shm_mem;
 	buf->shm_fd 	= shm_fd;
 	buf->front	= 0; 
 	buf->rear	= -1;
@@ -38,7 +38,6 @@ void init_cir_Buf(CQUEUE *buf, int shm_fd, void *shm_mem)
 int SOI_Create(const char *shm_name, int Payload_Size, int flag)
 {
 	int shm_fd;
-	void *shm_mem;
 	void *cirque_ds;
 
 	/* Create the shared memory object */ 
@@ -56,23 +55,24 @@ int SOI_Create(const char *shm_name, int Payload_Size, int flag)
 		ftruncate(shm_fd, SOI_SHMSIZE);
 
 	/* map the shared memory segment to the address space of the process */
-	shm_mem = mmap(0, SOI_SHMSIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
-	if (shm_mem == MAP_FAILED)
+	Shm_Ptr.shm_mem = mmap(0, SOI_SHMSIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+
+	if (Shm_Ptr.shm_mem == MAP_FAILED)
 	{
 		printf("mmap error\n");
 		exit(1);
 	}
+	printf("shared memory attached at address %p\n",Shm_Ptr.shm_mem);
 
-	cirque_ds = shm_mem + (Payload_Size * ((SOI_SHMSIZE/Payload_Size) - 1));
+	cirque_ds = Shm_Ptr.shm_mem + (Payload_Size * ((SOI_SHMSIZE/Payload_Size) - 1));
 	Cir_Que_Ptr = (CQUEUE *)cirque_ds;
-	Cir_Que_Ptr->shm_mem = shm_mem;
-	Cir_Que_Ptr->curr_seg = Cir_Que_Ptr->shm_mem;
+	Shm_Ptr.curr_seg = Shm_Ptr.shm_mem;
 
 	if(flag != SOI_ATTACH)
 	{
-		init_cir_Buf(Cir_Que_Ptr, shm_fd, shm_mem);
+		init_cir_Buf(Cir_Que_Ptr, shm_fd);
 		Cir_Que_Ptr->payload_size = Payload_Size;
-		Cir_Que_Ptr->queue_size   = (SOI_SHMSIZE/Payload_Size) -1;
+		Cir_Que_Ptr->queue_size   = (SOI_SHMSIZE/Payload_Size) - 1;
 	}
 	
 	return Cir_Que_Ptr->shm_fd;
@@ -91,13 +91,14 @@ int SOI_Send(int id, void *send_buf)
 			rc = EQFULL;  
 		else
 		{
+			printf("\nfront = %d rear = %d",Cir_Que_Ptr->front, Cir_Que_Ptr->rear);
 			Cir_Que_Ptr->rear = (Cir_Que_Ptr->rear+1) % Cir_Que_Ptr->queue_size; 
 			if(Cir_Que_Ptr->rear == 0)
 			{
-				Cir_Que_Ptr->curr_seg = Cir_Que_Ptr->shm_mem;
+				Shm_Ptr.curr_seg = Shm_Ptr.shm_mem;
 			}
-			memcpy (Cir_Que_Ptr->curr_seg, send_buf, Cir_Que_Ptr->payload_size);
-			Cir_Que_Ptr->curr_seg += Cir_Que_Ptr->payload_size;
+			memcpy (Shm_Ptr.curr_seg, send_buf, Cir_Que_Ptr->payload_size);
+			Shm_Ptr.curr_seg += Cir_Que_Ptr->payload_size;
 	
 			if(sizeof(send_buf) > Cir_Que_Ptr->payload_size)
 				rc = EMOVERFLOW;
@@ -116,7 +117,6 @@ int SOI_Recv(int id, void *recv_buf, int msg_flg)
 {
 	int rc = ESUCCESS;
 
-	printf("\nshm_fdfromP2:%d\n",Cir_Que_Ptr->shm_fd);
 	printf("\nfront=%d rear=%d\n",Cir_Que_Ptr->front,Cir_Que_Ptr->rear);
 	if(id != Cir_Que_Ptr->shm_fd)
 		rc = EINVALID;
@@ -128,15 +128,15 @@ int SOI_Recv(int id, void *recv_buf, int msg_flg)
 		{
 			if (msg_flg == SOI_LASTWMSG)
 			{
-				memcpy (recv_buf,(Cir_Que_Ptr->shm_mem + (Cir_Que_Ptr->payload_size * Cir_Que_Ptr->rear)), Cir_Que_Ptr->payload_size);
+				memcpy (recv_buf,(Shm_Ptr.shm_mem + (Cir_Que_Ptr->payload_size * Cir_Que_Ptr->rear)), Cir_Que_Ptr->payload_size);
 				Cir_Que_Ptr->front	= 0; 
 				Cir_Que_Ptr->rear	= -1;
 				Cir_Que_Ptr->count	= 0;
-				Cir_Que_Ptr->curr_seg	= Cir_Que_Ptr->shm_mem;
+				Shm_Ptr.curr_seg	= Shm_Ptr.shm_mem;
 			}
 			else
 			{
-				memcpy (recv_buf,(Cir_Que_Ptr->shm_mem + (Cir_Que_Ptr->payload_size * Cir_Que_Ptr->front)), Cir_Que_Ptr->payload_size);
+				memcpy (recv_buf,(Shm_Ptr.shm_mem + (Cir_Que_Ptr->payload_size * Cir_Que_Ptr->front)), Cir_Que_Ptr->payload_size);
 				Cir_Que_Ptr->front = (Cir_Que_Ptr->front+1) % Cir_Que_Ptr->queue_size;
 				Cir_Que_Ptr->count--;
 			}
@@ -155,16 +155,16 @@ int SOI_Destroy(int id)
 		rc = EINVALID;
 	else
 	{
-		printf ("shared memory attached at address %p\n", Cir_Que_Ptr->shm_mem);
-
 		/* Detach the shared memory segment */
-		rc = munmap(Cir_Que_Ptr->shm_mem, SOI_SHMSIZE);
+		rc = munmap(Shm_Ptr.shm_mem, SOI_SHMSIZE);
 		if (rc == -1) 
 			printf("SHM Unmap failed");
 
-		/* Deallocate the shared memory segment.  */ 
+		/* Deallocate the shared memory segment */ 
 		//shmctl (Cir_Que_Ptr->shm_fd, IPC_RMID, 0); 
 	}
 	return rc;
 }
+
+
 

3d01042f85a11ffdbdfdb7cb7747c1f2687dd898
SOI2016 <srinivas_r@hcl.com>
2016-07-21 19:47:38 +0530

    Add files via upload

---
+++ Project/TestFiles/shmp8.c
@@ -0,0 +1,48 @@
+#include "shm_que.h"
+
+/* Wrapper function to test SOI APIs */
+int main(int argc , char *argv[])
+{
+	int shm_fd;
+	const char *shm_name = "/SOI-SHM4";
+	int brk = 1;
+        int option = 0;
+        int ret = ESUCCESS;
+	char recv_buf[128];
+
+	shm_fd = SOI_Create(shm_name, SOI_PAYLOAD_128, SOI_ATTACH);        
+	printf("1.To read oldest unread message\n");
+	printf("2.To read the latest message\n");
+        printf("3.To exit\n");
+	while(brk)
+        { 
+           
+		printf("\nEnter your option:");
+		scanf("%d",&option);
+		switch(option)
+		{
+		case 1:
+			ret = SOI_Recv(shm_fd, recv_buf, 0);	    
+			if(ret != ESUCCESS)
+				printf("\nRead failed with error code:%d",ret);
+			else
+				printf("\nrecv_mesg=%s\n",recv_buf);
+		break;
+                case 2:
+			ret = SOI_Recv(shm_fd, recv_buf, SOI_LASTWMSG);	    
+			if(ret != ESUCCESS)
+				printf("\nRead failed with error code:%d",ret);
+			else
+				printf("\nrecv_mesg=%s\n",recv_buf);
+		break;
+		case 3:
+			brk = 0;
+			SOI_Destroy(shm_fd, shm_name);
+		break; 
+		default: 
+			printf("\nInvalid Option");
+		break; 
+		} 
+	}
+	return 0;
+}

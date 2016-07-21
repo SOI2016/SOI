c44dd10d06a7eda14da2d940dba4edaedd696a68
SOI2016 <srinivas_r@hcl.com>
2016-07-21 19:30:09 +0530

    Add files via upload

---
+++ Project/SourceCode/shm_que.c
@@ -0,0 +1,198 @@
+#include "shm_que.h"
+ 
+CQUEUE *Cir_Que_Ptr;
+SHMPTR Shm_Ptr;
+
+/* Function to check whether the Circular Queue is Full or Not */
+int isFull(CQUEUE *buf)
+{
+	int rc = ESUCCESS;
+
+	if(buf->count == buf->queue_size)
+        	rc = EQFULL;
+
+ 	return rc;
+}
+
+/* Function to check whether the Circular Queue is Empty or Not */
+int isEmpty(CQUEUE *buf)
+{
+	int rc = ESUCCESS;
+	
+	if(buf->count == 0)
+		rc = EQEMPTY;
+	 
+	return rc;
+}
+
+/* Function to initialize the Circular Queue */
+void init_cir_Buf(CQUEUE *buf, int shm_fd)
+{
+	buf->shm_fd 	= shm_fd;
+	buf->front	= 0; 
+	buf->rear	= -1;
+	buf->count	= 0;
+}
+
+/* API to create SOI object */
+int SOI_Create(const char *shm_name, int Payload_Size, int flag)
+{
+	int shm_fd;
+	void *cirque_ds;
+	Shm_Ptr.isAttach = 0;
+
+	if(flag != SOI_ATTACH)
+	{
+		/* Create the shared memory object */ 
+		shm_fd = shm_open(shm_name, O_CREAT | O_RDWR, 0666);
+		if(shm_fd == -1)
+		{
+			printf("shm_open error\n");
+			exit(1);
+		}
+		else
+			printf("\nCreated SHM_FD:%d\n",shm_fd);
+
+		/* configure the size of the shared memory segment */
+		if (ftruncate(shm_fd, SOI_SHMSIZE) == -1)
+		{
+			printf("shm_open error\n");
+			exit(1);
+		}
+
+		/* map the shared memory segment to the address space of the process */
+		Shm_Ptr.shm_mem = mmap(0, SOI_SHMSIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+	}
+	else
+	{
+		Shm_Ptr.isAttach = SOI_ATTACH;
+		/* Create the shared memory object */ 
+		shm_fd = shm_open(shm_name, O_RDWR, 0666);
+		if(shm_fd == -1)
+		{
+			printf("shm_open error\n");
+			exit(1);
+		}
+		else
+			printf("\nExisting SHM_FD:%d\n",shm_fd);
+
+		/* map the shared memory segment to the address space of the process */
+		Shm_Ptr.shm_mem = mmap(0, SOI_SHMSIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+	}
+
+	if (Shm_Ptr.shm_mem == MAP_FAILED)
+	{
+		printf("mmap error\n");
+		exit(1);
+	}
+	printf("shared memory attached at address %p\n",Shm_Ptr.shm_mem);
+
+	cirque_ds = Shm_Ptr.shm_mem + (Payload_Size * ((SOI_SHMSIZE/Payload_Size) - 1));
+	Cir_Que_Ptr = (CQUEUE *)cirque_ds;
+	Shm_Ptr.curr_seg = Shm_Ptr.shm_mem;
+
+	if(Shm_Ptr.isAttach != SOI_ATTACH)
+	{
+		init_cir_Buf(Cir_Que_Ptr, shm_fd);
+		Cir_Que_Ptr->payload_size = Payload_Size;
+		Cir_Que_Ptr->queue_size   = (SOI_SHMSIZE/Payload_Size) - 1;
+	}
+	
+	return Cir_Que_Ptr->shm_fd;
+}
+
+/* API to send message into SOI object */
+int SOI_Send(int shm_id, void *send_buf)
+{
+	int rc = ESUCCESS;
+
+	if(shm_id != Cir_Que_Ptr->shm_fd)
+		rc = EINVALID;
+	else
+	{
+		if(isFull(Cir_Que_Ptr) == EQFULL)
+			rc = EQFULL;  
+		else
+		{
+			printf("\nfront = %d rear = %d",Cir_Que_Ptr->front, Cir_Que_Ptr->rear);
+
+			Cir_Que_Ptr->rear = (Cir_Que_Ptr->rear+1) % Cir_Que_Ptr->queue_size; 
+			if(Cir_Que_Ptr->rear == 0)
+				Shm_Ptr.curr_seg = Shm_Ptr.shm_mem;
+
+			memcpy (Shm_Ptr.curr_seg, send_buf, Cir_Que_Ptr->payload_size);
+			Shm_Ptr.curr_seg += Cir_Que_Ptr->payload_size;
+	
+			if(sizeof(send_buf) > Cir_Que_Ptr->payload_size)
+				rc = EMOVERFLOW;
+
+			Cir_Que_Ptr->count++;
+
+			printf("\nfront = %d rear = %d",Cir_Que_Ptr->front, Cir_Que_Ptr->rear);
+		}
+	}
+	return rc;
+}
+
+/* API to receive message from SOI object */
+int SOI_Recv(int shm_id, void *recv_buf, int msg_flg)
+{
+	int rc = ESUCCESS;
+
+	printf("\nfront=%d rear=%d\n",Cir_Que_Ptr->front,Cir_Que_Ptr->rear);
+
+	if(shm_id != Cir_Que_Ptr->shm_fd)
+		rc = EINVALID;
+	else
+	{
+		if(isEmpty(Cir_Que_Ptr) == EQEMPTY)
+			rc = EQEMPTY;
+		else
+		{
+			if (msg_flg == SOI_LASTWMSG)
+			{
+				memcpy (recv_buf,(Shm_Ptr.shm_mem + (Cir_Que_Ptr->payload_size * Cir_Que_Ptr->rear)), Cir_Que_Ptr->payload_size);
+				Cir_Que_Ptr->front	= 0; 
+				Cir_Que_Ptr->rear	= -1;
+				Cir_Que_Ptr->count	= 0;
+				Shm_Ptr.curr_seg	= Shm_Ptr.shm_mem;
+			}
+			else
+			{
+				memcpy (recv_buf,(Shm_Ptr.shm_mem + (Cir_Que_Ptr->payload_size * Cir_Que_Ptr->front)), Cir_Que_Ptr->payload_size);
+				Cir_Que_Ptr->front = (Cir_Que_Ptr->front+1) % Cir_Que_Ptr->queue_size;
+				Cir_Que_Ptr->count--;
+			}
+		}
+	}
+	printf("\nfront=%d rear=%d\n",Cir_Que_Ptr->front,Cir_Que_Ptr->rear);
+
+	return rc;
+}
+
+/* API to destroy SOI object */
+int SOI_Destroy(int shm_id, const char *shm_name)
+{
+	int rc = ESUCCESS;
+
+	if(shm_id != Cir_Que_Ptr->shm_fd)
+		rc = EINVALID;
+	else
+	{
+		/* Detach the shared memory segment */
+		rc = munmap(Shm_Ptr.shm_mem, SOI_SHMSIZE);
+		if (rc == -1) 
+			printf("SHM Unmap failed");
+
+		if(Shm_Ptr.isAttach != SOI_ATTACH)
+		{
+			/* Deallocate the shared memory segment */
+			rc = shm_unlink(shm_name);
+			if (rc == -1) 
+				printf("SHM Unlink failed");
+		}
+	}
+	return rc;
+}
+
+
